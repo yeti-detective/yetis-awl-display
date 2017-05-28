e28a4486c287194573ea37eb028047c67a9b4e1c
yeti-detective <yeti.detective@gmail.com>
2017-05-28 12:57:28 -0400

    style it like a dude

--- components/Exposition.jsx
+++ components/Exposition.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+var aStyle = {
+ color: "#801515"
+}
+
 // Rendered by Container
 function Exposition(){
   return(
     <div>
       <h2>Use My Wishlist API</h2>
-      <p><a href="http://yetis-amazon-wishlist-api.herokuapp.com/" target="blank">Here</a> is the instruction page.</p>
-      <p><a href="https://github.com/yeti-detective/yet-azwl-api" target="blank">Here</a>'s the repository if you'd like to contribute. (It still needs the capability to import prices)</p>
+      <p><a style={aStyle} href="http://yetis-amazon-wishlist-api.herokuapp.com/" target="blank">Here</a> is the instruction page.</p>
+      <p><a style={aStyle} href="https://github.com/yeti-detective/yet-azwl-api" target="blank">Here</a>'s the repository if you'd like to contribute. (It still needs the capability to import prices)</p>
       <p>If you're just here for the demo, put your (publically viewable) Amazon Wishlist ID in the input box below.</p>
     </div>
   )
--- containers/Container.jsx
+++ containers/Container.jsx
@@ -7,6 +7,13 @@ import Form from '../components/Form.jsx'
 import Exposition from '../components/Exposition.jsx'
 import List from '../components/List.jsx'
 
+var style = {
+  width: "70%",
+  padding: 15,
+  alignText: "center",
+  fontFamily: "'Kumar One', cursive"
+}
+
 class Container extends React.Component {
   constructor(){
     super()
@@ -30,7 +37,7 @@ class Container extends React.Component {
 
   render(){
     return (
-      <div>
+      <div style={style}>
         <Exposition />
         <Form getList={this.getList} />
         {this.state.renderList ? <List url={this.state.url} list={this.state.wishlist}/> : null}
